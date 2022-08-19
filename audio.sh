0ed6011206ab09dbdbd0bfbe23cc8ce79b77e647
DarmExplosion <87719681+DarmExplosion@users.noreply.github.com>
2022-08-19 20:50:41 +0200

    Add files via upload

---
+++ audio.sh
@@ -0,0 +1,7 @@
+echo "Remove junk audio"
+sudo apt remove totem -y &&
+sudo apt remove brasero -y &&
+sudo apt remove cheese -y &&
+sudo apt remove sound-juicer -y &&
+sudo apt remove gnome-sound-recorder -y &&
+exit
